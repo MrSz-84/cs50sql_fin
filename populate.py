f01021c6416d69eebab9a660d1968ef374057a51
Marcin Borowski <pixelmotionhd@gmail.com>
2024-02-27 22:17:09 +0000

    DB populate testing in progress

--- populate.py
+++ populate.py
@@ -5,25 +5,74 @@ import pandas as pd
 import numpy as np
 
 
-# def add_one_field(data, table_name, field_name):
+def add_one_field(data, table_name, field_name):
+    query = sql.SQL("INSERT INTO {table} ({field}) VALUES (%s)")
 
-#     query = sql.SQL("INSERT INTO {table} ({field}) VALUES (%s)")
-
-#     for elem in data:
-#         cur.execute(
-#             query.format(
-#                 table=sql.Identifier(f'{table_name}'),
-#                 field=sql.Identifier(f'{field_name}')), (elem,)
-#         )
-#     conn.commit()
-
-# def iter_over_inputs(data_set):
+    for elem in data:
+        cur.execute(
+            query.format(
+                table=sql.Identifier(f'{table_name}'),
+                field=sql.Identifier(f'{field_name}')), (elem,)
+        )
+    conn.commit()
     
-#     for elem in data_set:
-#         data = elem['data']
-#         table = elem['table']
-#         field = elem['field']
-#         add_one_field(data, table, field)
+def iter_over_inputs(data_set):
+    
+    for elem in data_set:
+        data = elem['data']
+        table = elem['table']
+        field = elem['field']
+        add_one_field(data, table, field)
+
+def add_3_fields(data_set):
+    col = data_set['data'].columns.values.tolist()
+    
+    query = sql.SQL("INSERT INTO {table} ({field1}, {field2}, {field3}) VALUES (%s, %s ,%s)")
+
+    for _, elem in data_set['data'].iterrows():
+        cur.execute(
+            query.format(
+                table=sql.Identifier(data_set['table']),
+                field1=sql.Identifier(data_set['fields'][0]),
+                field2=sql.Identifier(data_set['fields'][1]),
+                field3=sql.Identifier(data_set['fields'][2])), 
+                (elem[col[0]], elem[col[1]], elem[col[2]],)
+        )
+        
+    conn.commit()
+
+def get_id_for_submediums():
+    submediums = df[['submedium', 'wydawca_nadawca', 'zasięg medium']].sort_values(by='submedium')
+    submediums.drop_duplicates(subset=['submedium'], keep='first', inplace=True, ignore_index=True)
+    submediums.index = submediums.index + 1
+    
+    if sum(submediums.value_counts()) != submediums.index.max():
+        exit('Max index inni niż długość listy.')
+    
+    query1 = sql.SQL('SELECT {fields} FROM {table}').format(
+    fields=sql.SQL(',').join([
+        sql.Identifier('broadcaster'),
+        sql.Identifier('id')
+    ]),
+    table=sql.Identifier('broadcasters'))
+    cur.execute(query1)
+    broadcasters = dict(cur.fetchall())
+
+    query2 = sql.SQL('SELECT {fields} FROM {table}').format(
+        fields=sql.SQL(',').join([
+            sql.Identifier('reach'),
+            sql.Identifier('id')
+        ]),
+        table=sql.Identifier('ad_reach'))
+    cur.execute(query2)
+    ad_reach = dict(cur.fetchall())
+
+    submediums['wydawca_nadawca'] = submediums['wydawca_nadawca'].map(broadcasters)
+    submediums['zasięg medium'] = submediums['zasięg medium'].map(ad_reach)
+    
+    return submediums
+
+
         
 
 # Reads the dataframe
@@ -33,32 +82,31 @@ df.reset_index(inplace=True)
 df.drop('index', axis=1, inplace=True)
 
 
-# dow2 = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek',
-#         'Sobota', 'Niedziela']
-# months = [
-#     'Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj',
-#     'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień',
-#     'Październik', 'Listopad', 'Grudzień'
-# ]
-# dates = df['data'].unique()
-# brands = df['brand'].sort_values().unique()
-# lengths = df['dł_ujednolicona'].sort_values().unique()
-# dayparts = df['daypart'].unique()
-# product_types = df['produkt(4)'].sort_values().unique()
-# broadcasters = df['wydawca_nadawca'].sort_values().unique()
-# reaches = df['zasięg medium'].unique()
+dow2 = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek',
+        'Sobota', 'Niedziela']
+months = [
+    'Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj',
+    'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień',
+    'Październik', 'Listopad', 'Grudzień'
+]
+dates = df['data'].unique()
+brands = df['brand'].sort_values().unique()
+lengths = df['dł_ujednolicona'].sort_values().unique()
+dayparts = df['daypart'].unique()
+product_types = df['produkt(4)'].sort_values().unique()
+broadcasters = df['wydawca_nadawca'].sort_values().unique()
+reaches = df['zasięg medium'].unique()
 
-# data_set = [{'data': dow2, 'table': 'pl_dow_names', 'field': 'dow_name'},
-#             {'data': months, 'table': 'pl_month_names', 'field': 'month_name'},
-#             {'data': dates, 'table': 'date_time', 'field': 'date'},
-#             {'data': brands, 'table': 'brands', 'field': 'brand'},
-#             {'data': lengths, 'table': 'unified_lenghts', 'field': 'length'},
-#             {'data': dayparts, 'table': 'dayparts', 'field': 'daypart'},
-#             {'data': product_types, 'table': 'product_types', 'field': 'product_type'},
-#             {'data': broadcasters, 'table': 'broadcasters', 'field': 'broadcaster'},
-#             {'data': reaches, 'table': 'ad_reach', 'field': 'reach'},
-#             ]
-
+data_set = [{'data': dow2, 'table': 'pl_dow_names', 'field': 'dow_name'},
+            {'data': months, 'table': 'pl_month_names', 'field': 'month_name'},
+            {'data': dates, 'table': 'date_time', 'field': 'date'},
+            {'data': brands, 'table': 'brands', 'field': 'brand'},
+            {'data': lengths, 'table': 'unified_lenghts', 'field': 'length'},
+            {'data': dayparts, 'table': 'dayparts', 'field': 'daypart'},
+            {'data': product_types, 'table': 'product_types', 'field': 'product_type'},
+            {'data': broadcasters, 'table': 'broadcasters', 'field': 'broadcaster'},
+            {'data': reaches, 'table': 'ad_reach', 'field': 'reach'},
+            ]
 
 # Openes connection to the DB
 conn = psycopg2.connect(
@@ -71,131 +119,11 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
+iter_over_inputs(data_set)
 
-# Commits day of week names into pl_dow_names
-dow2 = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek',
-        'Sobota', 'Niedziela']
-
-for day in dow2:
-    cur.execute(
-        sql.SQL("INSERT INTO {} (dow_name) VALUES (%s)")
-        .format(sql.Identifier('pl_dow_names')), (day,)
-    )
-conn.commit()
-
-
-# Commits months into pl_month_names
-months = [
-    'Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj',
-    'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień',
-    'Październik', 'Listopad', 'Grudzień'
-]
-
-query = sql.SQL("INSERT INTO {table} ({field}) VALUES (%s)")
-
-for month in months:
-    cur.execute(
-        query.format(
-            table=sql.Identifier('pl_month_names'),
-            field=sql.Identifier('month_name')), (month,)
-    )
-conn.commit()
-
-# Commits dates into date_time table
-dates = df['data'].unique()
-
-query = sql.SQL("INSERT INTO {table} ({field}) VALUES (%s)")
-
-for date in dates:
-    cur.execute(
-        query.format(
-            table=sql.Identifier('date_time'),
-            field=sql.Identifier('date')), (date,)
-    )
-conn.commit()
-
-# Commits brands into brands table.
-brands = df['brand'].sort_values().unique()
-
-query = sql.SQL('INSERT INTO {table} ({field}) VALUES(%s)')
-
-for brand in brands:
-    cur.execute(
-        query.format(
-            table=sql.Identifier('brands'),
-            field=sql.Identifier('brand')), (brand,)
-    )
-conn.commit()
-
-
-# Commits lengths into unified_length table.
-lengths = df['dł_ujednolicona'].sort_values().unique()
-
-query = sql.SQL('INSERT INTO {table} ({field}) VALUES (%s)')
-
-for length in lengths:
-    cur.execute(
-        query.format(
-            table=sql.Identifier('unified_lenghts'),
-            field=sql.Identifier('length')), (str(length),)
-    )
-conn.commit()
-
-
-# Commits dayparts into dayparts table.
-dayparts = df['daypart'].unique()
-
-query = sql.SQL('INSERT INTO {table} ({field}) VALUES (%s)')
-
-for daypart in dayparts:
-    cur.execute(
-        query.format(
-            table=sql.Identifier('dayparts'),
-            field=sql.Identifier('daypart')), (str(daypart),)
-    )
-conn.commit()
-
-
-# Commits products into product_types table.
-product_types = df['produkt(4)'].sort_values().unique()
-
-query = sql.SQL('INSERT INTO {table} ({field}) VALUES (%s)')
-
-for product in product_types:
-    cur.execute(
-        query.format(
-            table=sql.Identifier('product_types'),
-            field=sql.Identifier('product_type')), (str(product),)
-    )
-conn.commit()
-
-
-# Commits broadcaster into broadcasters table.
-broadcasters = df['wydawca_nadawca'].sort_values().unique()
-
-query = sql.SQL('INSERT INTO {table} ({field}) VALUES (%s)')
-
-for broadcaster in broadcasters:
-    cur.execute(
-        query.format(
-            table=sql.Identifier('broadcasters'),
-            field=sql.Identifier('broadcaster')), (str(broadcaster),)
-    )
-conn.commit()
-
-
-# Commits broadcaster into broadcasters table.
-reaches = df['zasięg medium'].unique()
-
-query = sql.SQL('INSERT INTO {table} ({field}) VALUES (%s)')
-
-for reach in reaches:
-    cur.execute(
-        query.format(
-            table=sql.Identifier('ad_reach'),
-            field=sql.Identifier('reach')), (str(reach),)
-    )
-conn.commit()
+submediums = get_id_for_submediums()
+data_set2 = [{'data': submediums, 'table': 'mediums', 'fields': ['submedium', 'broadcaster_id', 'ad_reach_id']}]
+add_3_fields(data_set2)
 
 
 conn.close()
