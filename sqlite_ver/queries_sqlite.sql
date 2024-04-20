d11d6d35612fa0e2bddca59f866ca447122bb09c
Marcin Borowski <pixelmotionhd@gmail.com>
2024-04-20 12:52:59 +0000

    updated queries according to new db structure

--- sqlite_ver/queries_sqlite.sql
+++ sqlite_ver/queries_sqlite.sql
@@ -51,113 +51,115 @@ WHERE "submedium" IN ('ESKA Wrocław', 'ZET', 'RMF FM', 'PR 1', 'TOK FM') AND "m
 -- Returns the complete data set for selected period of time, 
 -- for frurther processing in Pandas.
 SELECT "data_czas"."data" AS "data", "dzien", "dzien_tyg_nr" AS 'd_tyg_nr', "dzien_tyg", 
-"tydzien", "miesiac_nr" AS "m_nr", "miesiac", "rok", "spoty"."kod_reklamy" AS "kod_reklamy", "brand", 
-"submedium", "nadawca", "zasieg", "godz_bloku_rek" AS "godz_bloku", "daypart", 
-"dl_ujednolicona" AS "dl_spotu", "typ_produktu", "koszt", "typ", "l_emisji" AS "ilosc"
+"tydzien", "miesiac_nr" AS "m_nr", "miesiac", "rok", "kod_rek" AS "kod_reklamy", "brand", 
+"submedium", "nadawca", "zasieg", "godz_blok_rek" AS "godz_bloku", "daypart", 
+"dl_ujednolicona" AS "dl_spotu", "typ_produktu", "koszt", "typ_rek", "l_emisji" AS "ilosc"
 FROM "spoty"
 JOIN "data_czas" ON "data_czas"."data" = "spoty"."data"
 JOIN "brandy" ON "brandy"."id" = "spoty"."brand_id"
 JOIN "submedia" ON "submedia"."id" = "spoty"."submedium_id"
 JOIN "nadawcy" ON "nadawcy"."id" = "submedia"."nadawca_id"
 JOIN "zasiegi" ON "zasiegi"."id" = "submedia"."zasieg_id"
-JOIN "czasy_reklam" ON "czasy_reklam"."id" = "spoty"."czas_reklamy_id"
-JOIN "dayparty" ON "dayparty"."id" = "czasy_reklam"."daypart_id"
-JOIN "dl_ujednolicone" ON "dl_ujednolicone"."id" = "czasy_reklam"."dl_ujednolicona_id"
+JOIN "dayparty" ON "dayparty"."id" = "spoty"."daypart_id"
+JOIN "dl_ujednolicone" ON "dl_ujednolicone"."id" = "spoty"."dl_ujednolicona_id"
 JOIN "typy_produktu" ON "typy_produktu"."id" = "spoty"."typ_produktu_id"
 JOIN "dni_tyg" ON "dni_tyg"."id" = "data_czas"."dzien_tyg_nr"
 JOIN "miesiace" ON "miesiace"."id" = "data_czas"."miesiac_nr"
-WHERE "miesiac_nr" BETWEEN 8 AND 10 AND "rok" BETWEEN 2023 AND 2023;
+JOIN "bloki_rek" ON "bloki_rek"."id" = "spoty"."blok_rek_id"
+JOIN "kody_rek" ON "kody_rek"."id" = "spoty"."kod_rek_id"
+JOIN "typy_rek" ON "typy_rek"."id" = "spoty"."typ_rek_id"
+WHERE "miesiac_nr" BETWEEN 8 AND 8 AND "rok" BETWEEN 2023 AND 2023;
 
--- Insert entry for Polish dow names table
+-- Insert entry for dni_tyg table
 INSERT INTO "dni_ty" ("dzien_tyg") 
 VALUES ('Poniedziałek');
 
--- Insert entry for Polish month names table
+-- Insert entry for miesiace table
 INSERT INTO "miesiace" ("Miesiac") 
 VALUES ('Styczeń');
 
--- Insert entry for date_time table. Date format is ISO 8601.
+-- Insert entry for data_czas table. Date format is ISO 8601.
 INSERT INTO "data_czas" ("data") 
 VALUES ('2023-08-01');
 
--- Insert entry for brands table.
+-- Insert entry for brandy table.
 INSERT INTO "brandy" ("brand") 
 VALUES ('MEDIA MARKT');
 
--- Insert entry for unified_lengths table.
+-- Insert entry for dl_ujednolicone table.
 INSERT INTO "dl_ujednolicone" ("dl_ujednolicona") 
 VALUES (30);
 
--- Insert entry for dayparts table. 
+-- Insert entry for dayparty table. 
 -- "do 9" means in Polish language "up to 9 AM".
 INSERT INTO "dayparty" ("daypart") 
 VALUES ('do 9');
 
--- Insert entry for product_types table.
+-- Insert entry for typy_produktu table.
 -- "OGŁOSZENIA O PRACY" means in Polish language "Job annoucement" 
 -- or "Job advertisement".
 INSERT INTO "typy_produktu" ("typ_produktu") 
 VALUES ('OGŁOSZENIA O PRACY');
 
--- Insert entry for broadcaster table.
+-- Insert entry for nadawca table.
 INSERT INTO "nadawcy" ("nadawca") 
 VALUES ('BAR RADIO');
 
--- Insert entry for ad_reach table.
+-- Insert entry for zasiegi table.
 -- "krajowe" means in Polish language "nationwide" 
 INSERT INTO "zasiegi" ("zasieg") 
 VALUES ('krajowe');
 
--- Insert entry for mediums table.
+-- Insert entry for kody_rek table.
+INSERT INTO "kody_rek" ("kod_rek", "opis_spotu") 
+VALUES (22197532, 'Some text goses in here');
+
+-- Insert entry for blogi_rek table.
+
+INSERT INTO "bloki_rek" ("godz_blok_rek") 
+VALUES ('15:30-15:59');
+
+-- Insert entry for typy_rek table.
+INSERT INTO "typy_rek" ("typ_rek") 
+VALUES ('reklama');
+
+-- Insert entry for submedia table.
 INSERT INTO "submedia" ("submedium", "nadawca_id", "zasieg_id") 
 VALUES ('ESKA Katowice', 8, 2);
 
--- Insert entry for ad_time_details table.
-INSERT INTO "czasy_reklam" (
-    "data", 
-    "godz_bloku_rek", 
+-- Insert entry for spoty table. This is the main table.
+INSERT INTO "spoty" (
+    "data",
     "gg",
     "mm",
-    "dlugosc",
+    'ss',
+    "koszt",
+    "dlugosc" ,
+    "kod_rek_id",
     "daypart_id",
     "dl_ujednolicona_id",
-    "kod_rek"
-    ) 
-VALUES (
-    '2023-08-01', 
-    '8:00-8:29', 
-    8,
-    20,
-    29,
-    1,
-    3,
-    '2023-08-01 - 22194483 - 1'
-    );
-
--- Insert entry for ads_desc table. This is the main table.
-INSERT INTO "spoty" (
-    "data", 
-    "opis_rek", 
-    "kod_reklamy",
+    "blok_rek_id",
     "brand_id",
     "submedium_id",
-    "czas_reklamy_id",
     "typ_produktu_id",
-    "koszt",
     "l_emisji",
-    "typ"
+    "typ_rek_id",
     ) 
 VALUES (
-    '2023-08-01', 
-    'PATRZ BARBARA NO ALE..MEGA OKAZJE..SF SAMSUNG GALAXY M33 4XAP 5G..999ZŁ', 
-    22194483,
-    2,
-    78,
-    1,
-    2,
+    '2023-08-01',
+    12,
+    59,
+    25,
     310,
+    14,
+    2411,
+    2,
+    2,
+    14,
+    2,
+    56,
     1,
-    'reklama'
+    2'
     );
 
 
